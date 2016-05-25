a63b69161d67fd09c6e01ff308ddab764e261a21
rabarona <ricardo.barona@intel.com>
2016-05-25 13:21:54 -0500

    fixed elif

--- ipython/run_oa.sh
+++ ipython/run_oa.sh
@@ -17,7 +17,8 @@ echo "executing ops for date: $ndate"
 if [[ "$TYPE" == flow ]]; then
     echo `python2.7 lda_ranking.py --date $ndate --user ${LUSER} --ifile ${TYPE}_results.csv --ofile ${TYPE}_scores.csv --limit ${LIM}`
     echo `python2.7 add_nc_and_rep_services.py --date $ndate --user ${LUSER} --${TYPE}`
-    echo `python2.7 suspicious_connects_op.py --date $ndate --user ${LUSER}` elif [[ "$TYPE" == dns ]]; then
+    echo `python2.7 suspicious_connects_op.py --date $ndate --user ${LUSER}` 
+elif [[ "$TYPE" == dns ]]; then
     cd $dir/dns
     echo `python2.7 $dir/dns/dns_oa.py -d $ndate -i $dir/dns -l ${LIM}` else
     echo "data type not selected"
