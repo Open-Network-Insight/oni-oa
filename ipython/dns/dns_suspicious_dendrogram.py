bac18b73bcb41873fa1080339a029ee7f55377ad
elopezsa <everardo.lopez.sandoval@intel.com>
2016-05-30 15:58:48 -0500

    adding impala support for dns oa

--- ipython/dns/dns_suspicious_dendrogram.py
+++ ipython/dns/dns_suspicious_dendrogram.py
@@ -38,9 +38,24 @@ def get_dendro(dbase,ip_dst,year,month,day,storage_path):
 
     if not os.path.isfile("{0}dendro-{1}.csv".format(storage_path,ip_dst)):
 
-        dendro_qry = "hive -S -e \"set hive.cli.print.header=true; SELECT dns_a, dns_qry_name, ip_dst FROM (SELECT susp.ip_dst, susp.dns_qry_name, susp.dns_a FROM {0}.dns as susp  WHERE susp.y={1} AND susp.m={2} AND susp.d={3}  AND susp.ip_dst='{4}' ) AS tmp GROUP BY dns_a, dns_qry_name, ip_dst;\" | sed 's/[\\t]/,/g'> {5}dendro-{4}.csv".format(dbase,year,month,day,ip_dst,storage_path)
-
-        print dendro_qry
+        dendro_qry = "hive -S -e \"set hive.cli.print.header=true; \" | sed 's/[\\t]/,/g'> {5}dendro-{4}.csv".format(dbase,year,month,day,ip_dst,storage_path)
+        
+        dndro_qry = ("SELECT dns_a,
+        dns_qry_name, 
+        ip_dst 
+        FROM (
+            SELECT susp.ip_dst,
+            susp.dns_qry_name,
+            susp.dns_a 
+            FROM {0}.dns as susp  
+            WHERE susp.y={1} AND susp.m={2} AND susp.d={3}  AND susp.ip_dst='{4}' ) AS tmp 
+            GROUP BY dns_a, dns_qry_name, ip_dst")
+           
+    
+        dendro_file = "{0}dendro-{1}.csv".format(ip_dst,storage_path)
+        impala_cmd = "impala-shell -i {0} --print_header -B --output_delimiter='\\t' -q '{1}' -o {2}".format(impala_node,dns_details_qry,dendro_file)
+        
+        print impala_cmd
         subprocess.call(dendro_qry,shell=True)
 
 def validate_parameters(params):
