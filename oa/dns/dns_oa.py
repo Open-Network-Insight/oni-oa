aa331d40014a21fcea8cc80afc854f787ed0c8c7
elopezsa <everardo.lopez.sandoval@intel.com>
2016-06-23 18:11:47 -0500

    Adding start oa python script

---
+++ oa/dns/dns_oa.py
@@ -0,0 +1,9 @@
+
+
+class OA(object):
+
+    #def __init(self):
+
+    def start(self):
+        print "Iniziando DNS OA"    
+    
---
+++ oa/utils.py
@@ -0,0 +1,30 @@
+import logging
+
+class Util(object):
+
+    @classmethod
+    def create_logger(cls,looger_name,create_file=False):
+
+        # create logger for prd_ci
+		log = logging.getLogger('OA')
+		log.setLevel(level=logging.DEBUG)
+
+		# create formatter and add it to the handlers
+		formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+		if create_file:
+			# create file handler for logger.
+			fh = logging.FileHandler('oa.log')
+			fh.setLevel(level=logging.DEBUG)
+			fh.setFormatter(formatter)
+
+		# reate console handler for logger.
+		ch = logging.StreamHandler()
+		ch.setLevel(level=logging.DEBUG)
+		ch.setFormatter(formatter)
+
+		# add handlers to logger.
+		if create_file: log.addHandler(fh)
+		log.addHandler(ch)	
+
+		return  log
