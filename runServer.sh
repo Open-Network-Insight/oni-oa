956f6a46225f688b0d9dcba1cea34f4712610ee2
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2016-09-19 11:46:07 -0700

    Add suport for easymode on Jupyter

--- runServer.sh
+++ runServer.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-ipython notebook --no-mathjax --profile=ia --port=8889 --ip=0.0.0.0 --no-browser '--NotebookApp.extra_static_paths=["ui/ipython/"]'> ipython.out 2>&1&
+JUPYTER_VERSION=`jupyter --version 2> /dev/null`
+IPYTHON_VERSION=`ipython --version 2> /dev/null`
+
+# Is Jupyter installed?
+if [ "${JUPYTER_VERSION}" != "" ]; then
+    if [ ! -f ~/.jupyter/custom/custom.js ]; then
+        cp ui/ipython/custom/custom.* ~/.jupyter/custom/
+    fi
+    NOTEBOOK_CMD="jupyter notebook --no-mathjax --port=8889 --ip=0.0.0.0 --no-browser"
+elif [ "${IPYTHON_VERSION}" != "" ]; then
+    # Try with Ipython
+    NOTEBOOK_CMD="ipython notebook --no-mathjax --port=8889 --ip=0.0.0.0 --no-browser '--NotebookApp.extra_static_paths=[\"ui/ipython/\"]'"
+else
+    # Not installed, exit
+    echo "Jupyter/Ipython not found on your system."
+    exit
+fi
+
+eval "${NOTEBOOK_CMD} > ipython.out 2>&1&"
