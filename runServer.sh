965a6d04615e494bac7f80e45d42c49eb2cf5252
Diego Ortiz Huerta <diego.ortiz.huerta@intel.com>
2016-09-19 14:01:40 -0500

    fixup! Add suport for easymode on Jupyter

--- runServer.sh
+++ runServer.sh
@@ -4,9 +4,10 @@ IPYTHON_VERSION=`ipython --version 2> /dev/null`
 
 # Is Jupyter installed?
 if [ "${JUPYTER_VERSION}" != "" ]; then
-    if [ ! -f ~/.jupyter/custom/custom.js ]; then
-        cp ui/ipython/custom/custom.* ~/.jupyter/custom/
-    fi
+    # Grab latest version of easymode
+    mkdir ~/.jupyter/custom/ 2> /dev/null
+    cp ui/ipython/custom/* ~/.jupyter/custom/
+
     NOTEBOOK_CMD="jupyter notebook --no-mathjax --port=8889 --ip=0.0.0.0 --no-browser"
 elif [ "${IPYTHON_VERSION}" != "" ]; then
     # Try with Ipython
